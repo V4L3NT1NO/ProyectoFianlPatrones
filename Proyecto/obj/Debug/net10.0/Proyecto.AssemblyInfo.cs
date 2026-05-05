e049b057eaf2d32f8d3dc66be93e075695a988b0
Fabio Bolivar Jimenez <fabiobolivarj@gmail.com>
2026-05-05 17:21:52 -0400

    wea

--- Proyecto/obj/Debug/net10.0/Proyecto.AssemblyInfo.cs
+++ Proyecto/obj/Debug/net10.0/Proyecto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26e2537f24c7ac3374c8911277cf93a45cea2397")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+282fe05ba51422f7d1499a31c113337a7a75e3cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
